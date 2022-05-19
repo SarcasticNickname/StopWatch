dd09c70ad53da307b805e42cc87fddc38213adab
SarcasticNickname <78605109+SarcasticNickname@users.noreply.github.com>
2022-05-19 20:32:58 +0300

    initial interface, textview, 3 buttons.

---
+++ app/src/main/java/com/example/stopwatch/StopwatchActivity.java
@@ -0,0 +1,46 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean isRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+        String time = String.format(Locale.getDefault(),"%d:%02d:%02d", hours, minutes, secs);
+        timeView.setText(time);
+        if (isRunning) {
+            ++seconds;
+        }
+    }
+
+    public void onClickStart(View view) {
+        isRunning = true;
+    }
+
+    public void onClickStop(View view) {
+        isRunning = false;
+    }
+
+    public void onClickReset(View view) {
+        isRunning = false;
+        seconds = 0;
+    }
+}
