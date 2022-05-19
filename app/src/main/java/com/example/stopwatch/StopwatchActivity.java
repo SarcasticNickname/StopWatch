9c22a404a07e7a251414f9ab1cb224ecde692441
SarcasticNickname <78605109+SarcasticNickname@users.noreply.github.com>
2022-05-19 20:54:41 +0300

    Android handler, threads, handler.post, handler.postDelayed, timer.

--- app/src/main/java/com/example/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/stopwatch/StopwatchActivity.java
@@ -3,6 +3,7 @@ package com.example.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,18 +18,28 @@ public class StopwatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
 
     public void runTimer() {
-        final TextView timeView = (TextView) findViewById(R.id.time_view);
-        int hours = seconds / 3600;
-        int minutes = (seconds % 3600) / 60;
-        int secs = seconds % 60;
-        String time = String.format(Locale.getDefault(),"%d:%02d:%02d", hours, minutes, secs);
-        timeView.setText(time);
-        if (isRunning) {
-            ++seconds;
-        }
+        final TextView timeView = findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (isRunning) {
+                    ++seconds;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
     }
 
     public void onClickStart(View view) {
